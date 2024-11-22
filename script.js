419f04bc989efe9048a100de3c82f50ddca233a8
ArSharonin <ar.sharonin@gmail.com>
2024-11-22 17:52:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-let currentSlide = 0;
-
-function moveSlide(step) {
-    const slides = document.querySelectorAll(".carousel-images img");
-    const totalSlides = slides.length;
-
-    currentSlide += step;
-
-    if (currentSlide < 0) {
-        currentSlide = totalSlides - 1;
-    } else if (currentSlide >= totalSlides) {
-        currentSlide = 0;
-    }
-
-    const newTransform = -currentSlide * 100;
-    document.querySelector(".carousel-images").style.transform = `translateX(${newTransform}%)`;
-}
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // Циклическая прокрутка
+    autoplay: {
+        delay: 3000, // Автопрокрутка через 3 секунды
+        disableOnInteraction: false, // Останавливается, если пользователь взаимодействует
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
